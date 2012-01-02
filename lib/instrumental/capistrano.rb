30d183de95d8031a0695d290fa4b3d811bb8a10d
Chris <christopher@fastestforward.com>
2012-01-02 15:40:55 -0500

    check for existence of instance before attempting to load recipe in

--- lib/instrumental/capistrano.rb
+++ lib/instrumental/capistrano.rb
@@ -1,34 +1,36 @@
-defined?(Capistrano) && Capistrano::Configuration.instance.load do
-  namespace :instrumental do
-    namespace :util do
-      desc "marker for beginning of deploy"
-      task :deploy_start do
-        @instrumental_deploy_start = Time.now
+if Capistrano::Configuration.instance
+  Capistrano::Configuration.instance.load do
+    namespace :instrumental do
+      namespace :util do
+        desc "marker for beginning of deploy"
+        task :deploy_start do
+          @instrumental_deploy_start = Time.now
+        end
+
+        desc "marker for end of deploy"
+        task :deploy_end do
+          @instrumental_deploy_end = Time.now
+        end
       end
 
-      desc "marker for end of deploy"
-      task :deploy_end do
-        @instrumental_deploy_end = Time.now
+      desc "send a notice to instrumental about the deploy"
+      task :record_deploy_notice do
+        @instrumental_deploy_start ||= Time.now
+        @instrumental_deploy_end   ||= Time.now
+        deploy_duration_in_seconds = (@instrumental_deploy_end - @instrumental_deploy_start).to_i
+        deployer = Etc.getlogin.chomp
+        agent_options = {}
+        agent_options[:collector] = instrumental_host if exists?(:instrumental_host)
+        agent = Instrumental::Agent.new(instrumental_key, agent_options)
+        agent.synchronous = true
+        agent.notice("#{deployer} deployed #{current_revision}",
+                     @instrumental_deploy_start,
+                     deploy_duration_in_seconds)
       end
     end
 
-    desc "send a notice to instrumental about the deploy"
-    task :record_deploy_notice do
-      @instrumental_deploy_start ||= Time.now
-      @instrumental_deploy_end   ||= Time.now
-      deploy_duration_in_seconds = (@instrumental_deploy_end - @instrumental_deploy_start).to_i
-      deployer = Etc.getlogin.chomp
-      agent_options = {}
-      agent_options[:collector] = instrumental_host if exists?(:instrumental_host)
-      agent = Instrumental::Agent.new(instrumental_key, agent_options)
-      agent.synchronous = true
-      agent.notice("#{deployer} deployed #{current_revision}",
-                   @instrumental_deploy_start,
-                   deploy_duration_in_seconds)
-    end
+    before "deploy", "instrumental:util:deploy_start"
+    after  "deploy", "instrumental:util:deploy_end"
+    after  "instrumental:util:deploy_end", "instrumental:record_deploy_notice"
   end
-
-  before "deploy", "instrumental:util:deploy_start"
-  after  "deploy", "instrumental:util:deploy_end"
-  after  "instrumental:util:deploy_end", "instrumental:record_deploy_notice"
 end
