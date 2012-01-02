aaff8849f3f1cfc823cb109ff888084a033762a6
Joel Meador <joel@expectedbehavior.com>
2012-01-02 12:54:56 -0500

    better cap namespacing and automatic loading when gem is required

--- lib/instrumental_agent.rb
+++ lib/instrumental_agent.rb
@@ -1,1 +1,2 @@
 require 'instrumental/agent'
+require 'recipes/capistrano'
---
+++ lib/recipes/capistrano.rb
@@ -0,0 +1,34 @@
+Capistrano::Configuration.instance.load do
+  namespace :instrumental do
+    namespace :util do
+      desc "marker for beginning of deploy"
+      task :deploy_start do
+        @instrumental_deploy_start = Time.now
+      end
+
+      desc "marker for end of deploy"
+      task :deploy_end do
+        @instrumental_deploy_end = Time.now
+      end
+    end
+
+    desc "send a notice to instrumental about the deploy"
+    task :notice_deploy do
+      @instrumental_deploy_start ||= Time.now
+      @instrumental_deploy_end   ||= Time.now
+      deploy_duration_in_seconds = (@instrumental_deploy_end - @instrumental_deploy_start).to_i
+      deployer = Etc.getlogin.chomp
+      revision = deployed_revision
+      agent = Instrumental::Agent.new(instrumental_key,
+                                      :collector => collector_host)
+      agent.synchronous = true
+      agent.notice("#{deployer} deployed #{revision}",
+                   @instrumental_deploy_start,
+                   deploy_duration_in_seconds)
+    end
+  end
+
+  before "deploy", "instrumental:util:deploy_start"
+  after "deploy",  "instrumental:util:deploy_end"
+  after "instrumental:util:deploy_end", "instrumental:notice_deploy"
+end
