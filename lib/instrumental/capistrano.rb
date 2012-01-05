eb9e46b9e553e14f18942bf1258290c4f3bacda1
Elijah Miller <elijah.miller@gmail.com>
2012-01-05 18:00:21 -0500

    Fixing deploy duration tracking.

--- lib/instrumental/capistrano.rb
+++ lib/instrumental/capistrano.rb
@@ -7,26 +7,28 @@ if Capistrano::Configuration.instance
       namespace :util do
         desc "marker for beginning of deploy"
         task :deploy_start do
-          @instrumental_deploy_start = Time.now
+          set :instrumental_deploy_start, Time.now
         end
 
         desc "marker for end of deploy"
         task :deploy_end do
-          @instrumental_deploy_end = Time.now
+          set :instrumental_deploy_end, Time.now
         end
       end
 
       desc "send a notice to instrumental about the deploy"
       task :record_deploy_notice do
-        @instrumental_deploy_start ||= Time.now
-        @instrumental_deploy_end   ||= Time.now
-        deploy_duration_in_seconds = (@instrumental_deploy_end - @instrumental_deploy_start).to_i
+        start_at = exists?(:instrumental_deploy_start) ? instrumental_deploy_start : Time.now
+        end_at = exists?(:instrumental_deploy_end) ? instrumental_deploy_end : start_at
+        puts "@instrumental_deploy_start = #{start_at.inspect}"
+        puts "@instrumental_deploy_end = #{end_at.inspect}"
+        deploy_duration_in_seconds = end_at - start_at
         deployer = Etc.getlogin.chomp
         agent_options = { :synchronous => true }
         agent_options[:collector] = instrumental_host if exists?(:instrumental_host)
         agent = Instrumental::Agent.new(instrumental_key, agent_options)
         agent.notice("#{deployer} deployed #{current_revision}",
-                     @instrumental_deploy_start,
+                     start_at,
                      deploy_duration_in_seconds)
         logger.info("Notified Instrumental of deployment")
       end
