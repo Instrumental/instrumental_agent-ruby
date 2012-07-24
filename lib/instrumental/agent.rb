58dc0fe283a8f8b35026ebe65f410b1c30362b49
Elijah Miller <elijah.miller@gmail.com>
2012-07-24 13:58:35 -0400

    Move some complex system timer init code out of the agent.

--- lib/instrumental/agent.rb
+++ lib/instrumental/agent.rb
@@ -1,40 +1,9 @@
-require 'instrumental/rack/middleware'
 require 'instrumental/version'
+require 'instrumental/rack/middleware'
+require 'instrumental/system_timer'
 require 'logger'
 require 'thread'
 require 'socket'
-
-if RUBY_VERSION < "1.9" && RUBY_PLATFORM != "java"
-  timeout_lib = nil
-  ["SystemTimer", "system_timer"].each do |lib|
-    begin
-      unless timeout_lib
-        gem lib
-        require "system_timer"
-        timeout_lib  = SystemTimer
-      end
-    rescue Exception => e
-    end
-  end
-  if !timeout_lib
-    puts <<-EOMSG
-WARNING:: You do not currently have system_timer installed.
-It is strongly advised that you install this gem when using
-instrumental_agent with Ruby 1.8.x.  You can install it in
-your Gemfile via:
-gem 'system_timer'
-or manually via:
-gem install system_timer
-    EOMSG
-    require 'timeout'
-    InstrumentalTimeout = Timeout
-  else
-    InstrumentalTimeout = timeout_lib
-  end
-else
-  require 'timeout'
-  InstrumentalTimeout = Timeout
-end
 
 
 # Sets up a connection to the collector.
