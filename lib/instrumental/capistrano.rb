f24fd238fb53d551010d35bf8e53bd896f846326
Josh Miller <josh@sigstr.com>
2017-11-28 09:49:42 -0500

    Rename file to make more sense

--- lib/instrumental/capistrano.rb
+++ lib/instrumental/capistrano.rb
@@ -1,5 +1,5 @@
 if Gem::Specification.find_by_name("capistrano").version >= Gem::Version.new("3.0.0")
-  load File.expand_path("../capistrano/instrumental.rake", __FILE__)
+  load File.expand_path("../capistrano/capistrano3.rake", __FILE__)
 else
   require_relative "capistrano/capistrano2"
 end
